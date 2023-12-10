b3e612b27064194bcacc213ad538457296465fb5
WindWiperStudios <jacob@windwiper.site>
2023-12-09 18:22:23 -0600

    Fixed Screen Size Issue & Added HP System

---
+++ scenes/entities/EnemyParent.gd
@@ -0,0 +1,9 @@
+extends Node2D
+class_name EnemyParent
+
+@export var SpawnPoints : Array[Node2D]
+
+
+
+func SpawnEnemy():
+	pass
--- scenes/entities/entity.gd
+++ scenes/entities/entity.gd
@@ -4,22 +4,30 @@ class_name Entity
 @onready var spriteNode = $Sprite
 
 @onready var hp = $HPComponent
+@onready var hpBar = $ProgressBar
 
 @onready var root = get_tree().get_root()
 
 @export var itemDrops : Array[PackedScene]
+@export var dropParent : Node2D
 
 func _ready():
 	hp.DropItem.connect(DropWhenDead)
+	hpBar.max_value = hp.maxHP
+	hpBar.min_value = 0
 
+func _process(delta):
+	hpBar.value = hp.curHP
+	if hp.curHP < hp.maxHP:
+		hpBar.visible = true
+	else: hpBar.visible = false
 
 func DropWhenDead():
 	var root = get_tree().get_root()
 	for i in range(0, itemDrops.size()):
 		var item = itemDrops[i].instantiate()
-		print(item.name)
-		item.global_position = self.global_position
 		call_deferred("AddToRoot", item)
+		item.global_position = self.global_position
 
 func AddToRoot(itemDropped : Node):
-	root.add_child(itemDropped)
+	dropParent.add_child(itemDropped)
