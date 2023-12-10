0981796e904af625fe9c6be9b196473bb182a37e
AshTheWarmonger <kydrabusiness@gmail.com>
2023-12-10 02:30:52 -0500

    Fat UI update

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,10 +1,8 @@
 extends Control
+@onready var light2d = $PointLight2D
 
  
  
-func _ready():
-	pass
-	
 
 func _on_play_button_pressed()-> void:
 	var gamescreenScene = load("res://scenes/screens/game_screen.tscn")
@@ -17,3 +15,13 @@ func _on_settings_button_pressed():
 func _on_quit_pressed():
 	get_tree().quit()
 	
+
+#func _on_timer_timeout():
+#	var lightTimer = randf_range(0.1, 0.7)
+#	$Timer.wait_time = lightTimer
+#	print(lightTimer)
+#	lightLevelChanger()
+
+#func lightLevelChanger():
+#	light2d.energy = randf_range(0, 2)
+#	$Timer.start()
