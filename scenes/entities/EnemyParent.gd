854d8605c800227b6a84d5d738ff0819eb610b0e
WindWiperStudios <jacob@windwiper.site>
2023-12-09 18:40:18 -0600

    EnemyParentChanges

--- scenes/entities/EnemyParent.gd
+++ scenes/entities/EnemyParent.gd
@@ -1,9 +1,13 @@
 extends Node2D
 class_name EnemyParent
 
-@export var SpawnPoints : Array[Node2D]
+@export var spawnPoints : Array[Node2D]
+@export var enemyList : Array[PackedScene]
 
 
 
 func SpawnEnemy():
-	pass
+	var s = randi_range(0, spawnPoints.size())
+	var e = randi_range(0, enemyList.size())
+	
+	
