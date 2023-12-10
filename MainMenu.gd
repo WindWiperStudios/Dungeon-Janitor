47cd9d9314d5766619e4b809787808ea1205f25d
AshTheWarmonger <kydrabusiness@gmail.com>
2023-12-09 20:14:17 -0500

    Main Menu Update
    added a functional play button

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,9 +1,19 @@
 extends Control
 
-
+ 
  
 func _ready():
-	preload("res://scenes/screens/game_screen.tscn")
+	pass
 	
 
-#func  
+func _on_play_button_pressed()-> void:
+	var gamescreenScene = load("res://scenes/screens/game_screen.tscn")
+	get_tree().change_scene_to_file(gamescreenScene)
+	
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+	
+
+func _on_quit_pressed():
+	pass # Replace with function body.
+	
