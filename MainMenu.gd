fbdfc3afddebe5f38304a380bf89a59df45f22da
AshTheWarmonger <kydrabusiness@gmail.com>
2023-12-09 21:01:44 -0500

    Bug Fix
    -menu code fix

--- MainMenu.gd
+++ MainMenu.gd
@@ -8,7 +8,7 @@ func _ready():
 
 func _on_play_button_pressed()-> void:
 	var gamescreenScene = load("res://scenes/screens/game_screen.tscn")
-	get_tree().change_scene_to_file(gamescreenScene)
+	get_tree().change_scene_to_packed(gamescreenScene)
 	
 func _on_settings_button_pressed():
 	pass # Replace with function body.
