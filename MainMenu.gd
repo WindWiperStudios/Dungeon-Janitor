10bfabec88be9f7f3bbdf1bad366e34f51128396
AshTheWarmonger <kydrabusiness@gmail.com>
2023-12-11 17:39:24 -0500

    Main Menu alpha
    just incase i need a revert point

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,14 +2,18 @@ extends Control
 
 
 @onready var bgm = $BGM
+@onready var mouseLight = $PointLight2D
 
 var introLength : int = 128
+
 
 func _ready():
 	pass
 	
 
-func _process(delta):
+func _process(_delta):
+	mouseLight.global_position = get_global_mouse_position()
+	
 	if bgm.finished():
 		bgm.stream = "res://sounds/MainMenuBGMLoop.wav"
 
@@ -21,3 +25,7 @@ func _on_play_button_pressed()-> void:
 func _on_quit_pressed():
 	get_tree().quit()
 	
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
